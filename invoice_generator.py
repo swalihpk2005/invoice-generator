3f4eb7c361f072c047a57b0284dcc1d4e3171253
Your Name <your@email.com>
2026-09-04 11:18:58 +0530

    Add GST and gradtotal to invoice

--- invoice_generator.py
+++ invoice_generator.py
@@ -1,15 +1,41 @@
 print("==== INVOICE GENERATOR ====")
+items=[]
 customer_name=input("Enter name of the customer: ")
-product=input("Enter name of product: ")
-quantity=int(input("Quantity: "))
-unit_price=float(input("Price of One unit: "))
-total=quantity*unit_price
-print(f"Total: {total}")
+while True:
+    product=input("Enter name of product: ")
+    quantity=int(input("Quantity: "))
+    unit_price=float(input("Price of One unit: "))
+
+    total=quantity*unit_price
+
+    items.append({
+        "Product": product,
+        "Quantity": quantity,
+        "Price": unit_price,
+        "Total": total  
+    })
+
+    add_more=input("Add another product(yes/no): ").lower()
+    if add_more=="no":
+        break
+
+excluding_tax=0
+      
 
 print("\n========== INVOICE ==========")
 print(f"Customer : {customer_name}")
-print(f"Product  : {product}")
-print(f"Quantity : {quantity}")
-print(f"Unit Price: ₹{unit_price:.2f}")
-print(f"Total    : ₹{total:.2f}")
+print("----------------")
+for item in items:
+    print(f"Product  : {item['Product']}")
+    print(f"Quantity : {item['Quantity']}")
+    print(f"Unit Price: ₹{item['Price']:.2f}")
+    print(f"Total    : ₹{item['Total']:.2f}")
+    print("--------------")
+    excluding_tax+=item['Total']
+gst=(18/100) * excluding_tax
+grand_total=excluding_tax+ gst
+#note: 18% tax value indicate GST value for common products in India. 
+print(f"GST: ₹{gst:.2f}")
+print(f"Grand total: ₹{grand_total:.2f}")
+
 print("=============================")
